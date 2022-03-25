d4d80bc735b8d4c3e40b15a4f1628b152a7ab150
Scott Bailey <sbailey@anaconda.com>
2022-03-25 15:07:20 -0700

    Clean up CMake stuff.

--- recipe/build-liblief.sh
+++ recipe/build-liblief.sh
@@ -11,10 +11,19 @@ echo "Generating the build files."
 
 declare -a CMAKE_EXTRA_ARGS
 if [[ ${target_platform} == osx-* ]]; then
-  CMAKE_EXTRA_ARGS+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
+    CMAKE_EXTRA_ARGS+=(-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT})
+    CMAKE_EXTRA_ARGS+=(-DCMAKE_SKIP_RPATH=ON)
 elif [[ ${target_platform} == linux-ppc64le ]]; then
-  CMAKE_EXTRA_ARGS+=(-DLIEF_LOGGING=OFF)
+    CMAKE_EXTRA_ARGS+=(-DLIEF_LOGGING=OFF)
 fi
+
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    LIEF_TEST="OFF"
+else
+    LIEF_TEST="ON"
+fi
+
 
 cmake .. ${CMAKE_ARGS}               \
       -GNinja                        \
@@ -23,7 +32,7 @@ cmake .. ${CMAKE_ARGS}               \
       -DBUILD_STATIC_LIBS=OFF        \
       -DBUILD_SHARED_LIBS=ON         \
       -DLIEF_PYTHON_API=OFF          \
-      -DLIEF_TESTS=ON                \
+      -DLIEF_TESTS=${LIEF_TEST}      \
       -DLIEF_EXAMPLES=OFF            \
       -DLIEF_USE_CCACHE=OFF          \
       "${CMAKE_EXTRA_ARGS[@]}"       \
--- recipe/build-py-lief.sh
+++ recipe/build-py-lief.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -x
+# Isolate the build.
+mkdir -p build_py_${PY_VER}
+cd build_py_${PY_VER} || exit 1
+
+
+# Generate the build files.
+echo "Generating the build files."
 
 declare -a CMAKE_EXTRA_ARGS
 if [[ ${target_platform} =~ linux-* ]]; then
@@ -12,64 +18,44 @@ else
   exit 1
 fi
 
-# mkdir build-py${PY_VER}
-# pushd build-py${PY_VER}
 
-pushd build
+cmake .. ${CMAKE_ARGS}               \
+      -GNinja                        \
+      -DCMAKE_PREFIX_PATH=$PREFIX    \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DBUILD_STATIC_LIBS=OFF        \
+      -DBUILD_SHARED_LIBS=ON         \
+      -DLIEF_PYTHON_API=ON           \
+      -DLIEF_EXAMPLES=OFF            \
+      -DLIEF_USE_CCACHE=OFF          \
+      -DLIEF_INSTALL_PYTHON=ON       \
+      \
+      -DPYTHON_INCLUDE_DIR:PATH=$(${PYTHON} -c 'from sysconfig import get_paths; print(get_paths()["include"])')  \
+      -DPYTHON_LIBRARIES="${PREFIX}"/lib/libpython${PY_VER}.${SHLIB_EXT}  \
+      -DPYTHON_LIBRARY="${PREFIX}"/lib/libpython${PY_VER}.${SHLIB_EXT}  \
+      -DPYTHON_EXECUTABLE="${PREFIX}"/bin/python  \
+      -DPYTHON_VERSION=${PY_VER}     \
+      \
+      "${CMAKE_EXTRA_ARGS[@]}"       \
+      -DCMAKE_BUILD_TYPE=Release
 
-cmake .. -LAH -G "Ninja"  \
-  -DCMAKE_SKIP_RPATH=ON  \
-  -DCMAKE_AR="${AR}"  \
-  -DCMAKE_LINKER="${LD}"  \
-  -DCMAKE_NM="${NM}"  \
-  -DCMAKE_OBJCOPY="${OBJCOPY}"  \
-  -DCMAKE_OBJDUMP="${OBJDUMP}"  \
-  -DCMAKE_RANLIB="${RANLIB}"  \
-  -DCMAKE_STRIP="${STRIP}"  \
-  \
-  ${CMAKE_ARGS} \
-  \
-  -DCMAKE_BUILD_TYPE="Release"  \
-  -DBUILD_STATIC_LIBS=OFF  \
-  -DBUILD_SHARED_LIBS=ON  \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}"  \
-  -DCMAKE_BUILD_STATIC_LIBS=OFF  \
-  \
-  -DLIEF_PYTHON_API=ON  \
-  -DLIEF_INSTALL_PYTHON=ON  \
-  \
-  -DPYTHON_INCLUDE_DIR:PATH=$(${PYTHON} -c 'from sysconfig import get_paths; print(get_paths()["include"])')  \
-  -DPYTHON_LIBRARIES="${PREFIX}"/lib/libpython${PY_VER}.${SHLIB_EXT}  \
-  -DPYTHON_LIBRARY="${PREFIX}"/lib/libpython${PY_VER}.${SHLIB_EXT}  \
-  -DPYTHON_EXECUTABLE="${PREFIX}"/bin/python  \
-  -DPYTHON_VERSION=${PY_VER}  \
-  \
-  "${CMAKE_EXTRA_ARGS[@]}"
+# Build.
+echo "Building..."
+ninja pyLIEF || exit 1
 
-if [[ ! $? ]]; then
-  echo "configure failed with $?"
-  exit 1
-fi
+# Installing
+echo "Installing..."
+ninja install || exit 1
 
-# cmake --build . --target pyLIEF
-# cmake --build . --target install
-ninja -v pyLIEF
-if [[ ! $? ]]; then
-  echo "Build failed with $?"
-  exit 1
-fi
-ninja -v install
-if [[ ! $? ]]; then
-  echo "Install failed with $?"
-  exit 1
-fi
 ext_suffix="$( ${PYTHON} -c 'from sysconfig import get_config_var as get; print(get("EXT_SUFFIX") or get("SO"))' )"
 mv api/python/lief${EXT_SUFFIX} ${SP_DIR}/lief${ext_suffix}
 if [[ ${target_platform} == osx-* ]]; then
   ${INSTALL_NAME_TOOL:-install_name_tool} -id @rpath/_pylief${ext_suffix} ${SP_DIR}/lief${ext_suffix}
 fi
 
-${PYTHON} -c "import lief"
+${PYTHON} -c "import lief" || exit 1
+
+
 
 # conda run is broken. It does not remove the shell-script-added base-env PATH entries from the
 # front of PATH, so when it adds the new env, if there was *another* env activated, then that is
@@ -87,7 +73,3 @@ ${PYTHON} -c "import lief"
 #   echo "conda run ${PREFIX} --debug-wrapper-scripts python \"import lief\" runs the wrong python"
 #   exit 1
 # fi
-
-if [[ -d "${PREFIX}"/share/LIEF/examples ]]; then
-  rm -rf "${PREFIX}"/share/LIEF/examples/
-fi
